e0fe8256a5ceb1af437e4495c7552bec3d00f1e0
Eddie Tenz <tzz1002@gmail.com>
2023-10-28 12:45:16 +0800

    config

--- httpgin/config.go
+++ httpgin/config.go
@@ -1,12 +1,12 @@
 package httpgin
 
 type Config struct {
-	EnableAccess      bool   `yaml:"enable_access" default:"true"`
-	AccessLog         string `yaml:"access_log" default:"log"`
-	EnablePprof       bool   `yaml:"enable_pprof" default:"true"`
-	EnableMetrics     bool   `yaml:"enable_metrics" default:"true"`
-	EnableTraffic     bool   `yaml:"enable_traffic" default:"true"`
-	EnableHealthCheck bool   `yaml:"enable_health_check" default:"true"`
-	EnableTimeout     bool   `yaml:"enable_timeout" default:"true"`
-	Timeout           int    `yaml:"timeout" default:"10"` // seconds
+	EnableAccess      bool   `yaml:"enable_access" json:"enable_access" default:"true"`
+	AccessLog         string `yaml:"access_log" json:"access_log" default:"log"`
+	EnablePprof       bool   `yaml:"enable_pprof" json:"enable_pprof" default:"true"`
+	EnableMetrics     bool   `yaml:"enable_metrics" json:"enable_metrics" default:"true"`
+	EnableTraffic     bool   `yaml:"enable_traffic" json:"enable_traffic" default:"true"`
+	EnableHealthCheck bool   `yaml:"enable_health_check" json:"enable_health_check" default:"true"`
+	EnableTimeout     bool   `yaml:"enable_timeout" json:"enable_timeout" default:"true"`
+	Timeout           int    `yaml:"timeout" json:"timeout" default:"10"` // seconds
 }
