4d03e55c74f2ff342aee8cc44fade3578fbac6ae
Eddie <zhizhang.teng@shopee.com>
2023-11-01 19:20:02 +0800

    mock interface

--- httpcli/httpcli_test.go
+++ httpcli/httpcli_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/stretchr/testify/mock"
 	"net/http"
+	"net/url"
 	"reflect"
 	"testing"
 )
@@ -41,13 +42,17 @@ func Test_client_Request(t *testing.T) {
 				senderMock.On("Do", mock.Anything).Return(
 					nil,
 					fmt.Errorf("some error"),
-				)
+				).Once()
 			},
 			args: args{
 				ctx: func() context.Context {
 					return context.Background()
 				}(),
-				req: &http.Request{},
+				req: &http.Request{
+					Method: http.MethodPost,
+					URL:    &url.URL{},
+					Body:   http.NoBody,
+				},
 			},
 			wantResp: nil,
 			wantErr:  true,
@@ -59,7 +64,12 @@ func Test_client_Request(t *testing.T) {
 				sender: tt.fields.sender,
 				cfg:    tt.fields.cfg,
 			}
+
+			tt.behavior(tt.fields)
+
 			gotResp, err := c.Request(tt.args.ctx, tt.args.req)
+			t.Logf("gotResp: %v", gotResp)
+			t.Logf("err: %v", err)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Request() error = %v, wantErr %v", err, tt.wantErr)
 				return
